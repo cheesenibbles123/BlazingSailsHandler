90b5d190198263566c48910094f1b7b431332a7d
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-09-09 17:37:05 +0100

    Fixed issues with response timings and importing:"

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 const fetch = require('node-fetch');
-const { generateResponse } = require("./generateResponse");
-const types = require("./structs/types");
-const dataSorter = require("./sortDataForTypes");
+const { generateResponse } = require("./generateResponse.js");
+const types = require("./structs/types.js");
+const dataSorter = require("./sortDataForType.js");
 
 let apiKey;
+module.exports = {
+	init : (newApiKey) => {
+		apiKey = newApiKey;
+	},
+	query: (type,steamID) =>{
+		return new Promise((resolve,reject) => {
+			if (checkIfPublic(steamID)){
+				getStats(steamID).then(playerStats => {
 
-exports.init = function init(newApiKey){
-	apiKey = newApiKey;
-}
+					getDataForType(playerStats,type).then(response => {
 
-exports.query = async function query(type,steamID){
-	if (checkIfPublic(steamID)){
+						resolve(generateResponse(true,type,response));
+					}).catch(error => {
+						resolve(generateResponse(false,type,error));
+					});
 
-		getStats(steamID).then(playerStats => {
+				}).catch(error => {
+					resolve(generateResponse(false,type,error));
+				});
 
-			getDataForType(playerStats,type).then(response => {
-				return generateResponse(true,type,response);
-			}).catch(error => {
-				return generateResponse(false,type,error);
-			});
-
-		}).catch(error => {
-
-			return generateResponse(false,type,error);
-		})
-
-	}else{
-		return generateResponse(false,type,"Private profile.");
+			}else{
+				resolve(generateResponse(false,type,"Private profile."));
+			}
+		});
 	}
 }
 
 function checkIfPublic(steamID){
 	return new Promise ((resolve,reject) => {
-		fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${steamID}`).then(resp => resp.json()).then(response => {
+		fetch(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${apiKey}&steamids=${steamID}`).then(resp => resp.json()).then(response => {
 			if (response.response.players.communityvisibilitystate === 1){
 				resolve(false);
 			}else{
@@ -44,7 +45,7 @@ function checkIfPublic(steamID){
 
 function getStats(steamID){
 	return new Promise((resolve,reject) => {
-		fetch(`https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2?key=${steamKey}&appid=1158940&steamid=${steamID}`).then(resp => resp.text()).then(response => {
+		fetch(`https://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v2?key=${apiKey}&appid=1158940&steamid=${steamID}`).then(resp => resp.text()).then(response => {
 			if (response.includes("500 Internal Server Error")){
 				reject("Steam API error, code 500");
 			}else if (response.includes("Unknown problem determining WebApi request destination.")){
@@ -63,7 +64,7 @@ function getDataForType(playerStats,type){
 	return new Promise((resolve,reject) => {
 		switch (type){
 			case types.ACHIEVEMENTS:
-				resolve(dataSorter.achievements(playerstats));
+				resolve(dataSorter.achievements(playerStats));
 				break;
 			default:
 				reject("No such type found.");
