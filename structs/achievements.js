089d166b5b956e2aceb0769926771cb89b9d13a7
Cheesenibbles123 <56054349+cheesenibbles123@users.noreply.github.com>
2021-09-09 20:47:11 +0100

    Added missing achievements

--- structs/achievements.js
+++ structs/achievements.js
@@ -1,10 +1,18 @@
 module.exports = {
-	ACH_FIRST_STARTUP : "Landlubber No More",
+	ACH_CUSTOMIZE_CHARACTER : "More Than An Eyepatch",
+	ACH_CUSTOMIZE_SHIP : "Part Of The Ship",
+	ACH_DISCOVER_TEMPLECLUSTER : "Hell Is A Merrier Place",
 	ACH_DISCOVER_TRIDENTCLUSTER : "Wrath Of Poseidon",
 	ACH_DISCOVER_TROPICALCLUSTER : "Tropical Refuge",
-	ACH_DISCOVER_TEMPLECLUSTER : "Hell Is A Merrier Place",
-	ACH_CUSTOMIZE_SHIP : "Part Of The Ship",
-	ACH_CUSTOMIZE_CHARACTER : "More Than An Eyepatch",
+	ACH_FIRST_STARTUP : "Landlubber No More",
+	ACH_FIRST_VICTORY : "A Legend Begins",
+	ACH_HIDDENITEM_DUCK : "Quack!",
+	ACH_KILL_CANNON : "Collateral Danage",
+	ACH_KILL_REVENGE : "Eye For An Eye",
+	ACH_MATCH_MVP : "Pirate King",
 	ACH_PREMADE_CREW : "Part Of The Crew",
 	ACH_REFERENCE_CASTAWAY : "I Thought I Lost You",
+	ACH_SINK_REVENGE : "Beyond The Grave",
+	ACH_SWAP_TEAM : "More Like Guidelines",
+	ACH_TREASURECHEST : "Silver And Gold"
 }
